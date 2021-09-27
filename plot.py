4300298d05d094e5f363b73ff8849867fad99211
Andrew Fowlie <andrew.j.fowlie@qq.com>
2021-09-27 11:17:25 +0800

    tweaks to plot

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ Plot test-statistic distribution
 ================================
 """
 
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,27 +13,37 @@ from data import n_channels
 
 if __name__ == "__main__":
 
+    colors = colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+    palette = itertools.cycle(colors)
+
     plt.rc('text', usetex=True)
     plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
-    plt.rc('font', **{'family':'serif', 'size': 16})
+    plt.rc('font', **{'family':'serif', 'size': 18})
+    plt.subplots(figsize=(7, 5))
 
     test_statistic_global = np.load("data/test_statistic_global.npy")
     test_statistic_local = np.load("data/test_statistic_local.npy")
 
+    c = next(palette)
     plt.hist(test_statistic_local, bins='auto', density=True,
-             histtype="stepfilled", alpha=0.75, color="SeaGreen", label=r"Fixed mass, $m=151\,\text{GeV}$")
+             histtype="stepfilled", alpha=0.5, color=c,
+             label=r"Fixed mass, $m=151\,\text{GeV}$")
     plt.hist(test_statistic_global, bins='auto', density=True,
-             histtype="step", lw=2, label="Floating mass")
+             histtype="step", color=next(palette), lw=3,
+             label="Floating mass")
 
     x = np.linspace(0, 25, 10000)
-    plt.plot(x, pdf_half_chi2(x, 1), lw=2, label=r"$\frac12 \chi^2$ --- used in ref.~[1]")
-    plt.plot(x, pdf_half_chi2(x, n_channels), lw=2, color="DarkGreen", label=r"$\frac12 \chi^2_6$")
+    plt.plot(x, pdf_half_chi2(x, 1), color=next(palette), lw=2,
+             label=r"$\frac12 \chi^2$ --- used in ref.~[1]", zorder=-1)
+    plt.plot(x, pdf_half_chi2(x, n_channels), lw=2, color=c,
+             label=r"$\frac12 \chi^2_6$", zorder=-1)
 
-    plt.xlabel("$\lambda$")
+    plt.xlabel("Test-statistic, $\lambda$")
     plt.ylabel("PDF")
     plt.yticks([])
     plt.ylim(0, 0.5)
     plt.xlim(-1, 20)
 
-    plt.legend()
+    plt.legend(fancybox=False)
+    plt.tight_layout()
     plt.savefig("dist.pdf")
